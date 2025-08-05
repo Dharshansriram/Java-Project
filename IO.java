9d42f744a5e2e0a3b76fa693b5acbcc96fa686e9
Dharshansriram <dharshansriram.r@gmail.com>
2025-08-05 15:03:50 +0530

    Add files via upload

---
+++ IO.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IO {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Enter an integer: ");
+            int number = scanner.nextInt();
+            System.out.println("You entered: " + number);
+        } catch (Exception e) {
+            System.out.println("Input error: Please enter a valid integer.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
