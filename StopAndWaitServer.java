393bbcb2a1e600acf2eb52149e9cc3d94c4c42dd
dharshan <dharshansriram.r@gmail.com>
2025-08-25 15:02:21 +0530

    Adding

---
+++ StopAndWaitServer.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class StopAndWaitServer {
+public static void main(String[] args) throws Exception {
+DatagramSocket serverSocket = new DatagramSocket(9876);
+byte[] receiveData = new byte[1024];
+byte[] sendData;
+System.out.println("Server is running...");
+while (true) {
+DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+serverSocket.receive(receivePacket);
+String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+InetAddress clientIP = receivePacket.getAddress();
+int clientPort = receivePacket.getPort();
+System.out.println("Received Frame: " + sentence);
+if (new Random().nextInt(5) == 0) { 
+sendData = "NAK".getBytes(); 
+System.out.println("Sending NAK for: " + sentence);
+} else {
+sendData = "ACK".getBytes(); 
+System.out.println("Sending ACK for: " + sentence);
+}
+DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientIP, 
+clientPort);
+serverSocket.send(sendPacket);
+}
+}
+}
+
